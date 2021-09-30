5c4c1dc24a05e526b4d35c15bd9a04a12b1f2e81
pyjudychen <pyjudychen@gmail.com>
2021-09-30 16:51:05 -0700

    Finish Problem 1 to 3

--- week_2_homework.R
+++ week_2_homework.R
@@ -3,4 +3,7 @@ hw2 <- runif(50, 4, 50)
 hw2 <- replace(hw2, c(4,12,22,27), NA)
 
 hw2 = hw2[!is.na(hw2)]
-
+prob1 = subset(hw2, 14 <= hw2 & hw2 <= 38)
+times3 = prob1*3
+plus10 = times3 + 10
+plus10[seq(1,length(plus10),2)]
