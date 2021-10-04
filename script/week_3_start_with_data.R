657d11a564897a51a1c27ec949a061b7b2213868
pyjudychen <pyjudychen@gmail.com>
2021-10-04 15:10:48 -0700

    move to file script

---
+++ script/week_3_start_with_data.R
@@ -0,0 +1,30 @@
+surveys = read.csv('data/portal_data_joined.csv')
+nrow(surveys)
+ncol(surveys)
+
+class(surveys) # data.frame
+head(surveys) # look at top 6 rows of data
+tail(surveys) # look at bottom 6 rows of data
+View(surveys) # open data in another tab
+str(surveys) # view the structure of data
+summary(surveys)
+
+## Index (row first, then column)
+surveys[1, 1] # first row, first column
+surveys[1, ] # first row
+surveys[, 6] # sixth column (output: vector)
+surveys[6] # maintain data.frame structure
+class(surveys[, 6]) 
+class(surveys[6])
+
+surveys[1:6, ] # show first six rows
+surveys[-(1:6), ] # skip first six rows
+surveys[-1, ] # skip the first row
+
+colnames(surveys)
+surveys['species_id'] # maintain data.frame structure
+surveys[, 'species_id'] # output: vector
+class(surveys['species_id'])
+class(surveys[, 'species_id'])
+
+surveys$species_id # output: vector (preferalbe option)
