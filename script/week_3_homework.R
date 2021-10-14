8ab39dcf60f45e640da5d7445d80247d9790fe92
pyjudychen <pyjudychen@gmail.com>
2021-10-14 16:00:15 -0700

    week4 course

--- script/week_3_homework.R
+++ script/week_3_homework.R
@@ -5,19 +5,23 @@ surveys_base = data.frame(
   surveys$species_id, 
   surveys$weight, 
   surveys$plot_type)
+surveys_base = select(surveys, species_id, weight, plot_type)
  
 # Have this data frame only be the first 60 rows
 surveys_base = surveys_base[1:60, ]
+surveys_base = head(surveys_base, 60)
 
 # Convert both species_id and plot_type to factors.
-surveys_base$surveys.species_id = as.factor(
-  surveys_base$surveys.species_id)
-surveys_base$surveys.plot_type = as.factor(
-  surveys_base$surveys.plot_type)
+surveys_base$species_id = as.factor(surveys_base$species_id)
+surveys_base$plot_type = as.factor(surveys_base$plot_type)
 
 # Remove all rows where there is an NA in the weight column.
 surveys_base = na.omit(surveys_base)
+na.omit(surveys_base, surveys_base$weight)
+surveys_base[!is.na(surveys_base$weight),]
 
 # Create a second data frame (challenge_base) 
 # that only consists of individuals from surveys_base with weights > 150g
-challenge_base = subset(surveys_base, surveys_base$surveys.weight>150)
+challenge_base = subset(surveys_base, surveys_base$weight > 150)
+challenge_base = surveys_base[surveys_base$weight > 150, ]
+summary(challenge_base$weight)
--- script/week_4_data_manipulation.R
+++ script/week_4_data_manipulation.R
@@ -42,3 +42,37 @@ surveys %>%
 surveys %>%
   group_by(sex) %>%
   tally()
+
+#1. keep only observation before 1995
+surveys_base = filter(surveys, year < 1995)
+#2. retain only the columns year, sex, and weight
+surveys_challenge = select(surveys_base, year, sex, weight)
+#2-1. keep only observation before 1995 and retain only the columns year, sex, and weight
+surveys_challenge = surveys %>% 
+  filter(year < 1995) %>%
+  select(year, sex, weight)
+  
+# Create a new data frame from the surveys data that meets the following criteria: 
+#1. contains only the species_id column 
+#2. and a new column called hindfoot_half containing values that are half the hindfoot_length values. 
+#3. In this hindfoot_half column, there are no NAs and all values are less than 30. 
+#Name this data frame surveys_hindfoot_half
+colnames(surveys)
+
+surveys_hindfoot_half = surveys %>% 
+  filter(!is.na(hindfoot_length)) %>%
+  mutate(hindfoot_half = hindfoot_length/2) %>%
+  select(species_id, hindfoot_half) %>%
+  filter(hindfoot_half < 30)
+str(surveys_hindfoot_half)
+
+# Use group_by() and summarize() to find the mean, min, and max hindfoot length 
+# for each species (using species_id).
+surveys_hindfoot_summary= 
+  surveys %>%
+  filter(!is.na(hindfoot_length)) %>%
+  group_by(species_id) %>%
+  summarize(mean_length = mean(hindfoot_length), 
+            min_length = min(hindfoot_length), 
+            max_length = max(hindfoot_length)) 
+head(surveys_hindfoot_summary)
