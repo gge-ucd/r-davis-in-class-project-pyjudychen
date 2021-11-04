810ae026b7da51651fa4897420bd1c431e531890
pyjudychen <pyjudychen@gmail.com>
2021-11-04 11:47:36 -0700

    week7 preview and manipulate data

--- script/week_7_data_visualization_part2.R
+++ script/week_7_data_visualization_part2.R
@@ -1,6 +1,13 @@
 library(ggplot2)
+library(tidyverse)
 ggplot(diamonds, aes(x = clarity, fill = cut)) + 
   geom_bar()+
   theme(axis.text.x = element_text(angle = 70, vjust = 0.5)) +
   scale_fill_viridis_d(option = 'C') + 
   theme_classic()
+
+colnames(diamonds)
+diamonds_group = diamonds %>% 
+  group_by(cut) %>% 
+  summarize(avg_price = mean(price))
+# Visualization tips
